4a6082e1a258f315dd75028cd952bc26d9077f24
semaali159 <semaali1599@gmail.com>
2026-07-17 12:42:20 -0700

    fix: add services to module

--- src/Enrollments/Enrollment.module.ts
+++ src/Enrollments/Enrollment.module.ts
@@ -5,7 +5,13 @@ import { User } from "src/User/user.entity";
 import { EnrollmentService } from "./Enrollment.service";
 import { Enrollment } from "./Enrollment.entity";
 import { EnrollementController, MyEnrollmentsController } from "./Enrollment.controller";
+import { WaitlistService } from "./EnrollmentService/waitlist.service";
+import { EnrollmentQueryService } from "./EnrollmentService/EnrollmentQuery.service";
+import { EnrollmentAuthService } from "./EnrollmentService/EnrollmentAuth.service";
 
-@Module({imports:[TypeOrmModule.forFeature([Course,User,Enrollment])],exports:[EnrollmentService],
-    controllers:[EnrollementController,MyEnrollmentsController],providers:[EnrollmentService]})
+@Module({
+    imports:[TypeOrmModule.forFeature([Course,User,Enrollment])],
+    exports:[EnrollmentService],
+    controllers:[EnrollementController,MyEnrollmentsController],
+    providers:[EnrollmentService, WaitlistService, EnrollmentQueryService, EnrollmentAuthService]})
 export class EnrollmentModule{}
